61bc81b8f5eab5bd543419cbe24a8b905116dea0
elvesbd <elvesbd@yahoo.com>
2024-09-26 08:35:17 -0300

    chore: implementing use case find all tenants

--- src/core/tenant/ports/repository/tenant.repository.ts
+++ src/core/tenant/ports/repository/tenant.repository.ts
@@ -1,6 +1,7 @@
 import { Tenant } from '@core/tenant/model';
 
 export abstract class TenantRepository {
+  abstract findAll(): Promise<Tenant[]>;
   abstract save(tenant: Tenant): Promise<void>;
   abstract findByDocument(document: string): Promise<Tenant | null>;
 }
--- src/core/tenant/usecases/index.ts
+++ src/core/tenant/usecases/index.ts
@@ -1,1 +1,3 @@
+export * from './create/create-tenant.usecase';
 export * from './update/update-tenant.usecase';
+export * from './findAll/find-all-tenant.usecase';
