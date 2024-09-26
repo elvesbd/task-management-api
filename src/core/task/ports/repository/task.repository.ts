0a9879355e4a57d25463bfb70663e46339c2b892
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:21:23 -0300

    chore: implementing delete task use case on tenant

--- src/core/task/ports/repository/task.repository.ts
+++ src/core/task/ports/repository/task.repository.ts
@@ -2,6 +2,7 @@ import { Task } from '@core/task/model';
 
 export abstract class TaskRepository {
   abstract save(user: Task): Promise<void>;
+  abstract delete(id: string): Promise<void>;
   abstract findAll(tenantId: string): Promise<Task[]>;
   abstract findByIdAndTenantId(
     id: string,
