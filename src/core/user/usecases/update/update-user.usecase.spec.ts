47c54f5bd08c87be1d0ef83d3223537ca883fc2c
elvesbd <elvesbd@yahoo.com>
2024-09-26 09:59:44 -0300

    test: should throw NotFoundException if user not found

---
+++ src/core/user/usecases/update/update-user.usecase.spec.ts
@@ -0,0 +1,52 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { User } from '@core/user/model';
+import { UserRole } from '@core/user/enum';
+import { UpdateUserUseCase } from '@core/user/usecases';
+import { UserRepository } from '@core/user/ports/repository';
+import { UserDataBuilder } from '@test/__mocks__/data-builder/user';
+
+describe('UpdateUserUseCase', () => {
+  let sut: UpdateUserUseCase;
+  let userRepository: UserRepository;
+
+  const input = UserDataBuilder.aUser().build();
+  const user = User.create(input);
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const UserRepositoryProvider = {
+      provide: UserRepository,
+      useValue: {
+        save: jest.fn().mockResolvedValue(0),
+        findByEmail: jest.fn().mockResolvedValue(user),
+      },
+    };
+
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [UpdateUserUseCase, UserRepositoryProvider],
+    }).compile();
+
+    sut = app.get<UpdateUserUseCase>(UpdateUserUseCase);
+    userRepository = app.get<UserRepository>(UserRepository);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    const tenantId = '019229dc-e8c6-72cc-b599-c938df401967';
+
+    it('should throw NotFoundException if user not found', async () => {
+      jest.spyOn(userRepository, 'findByEmail').mockResolvedValueOnce(null);
+
+      await expect(sut.execute({ ...input, id: tenantId })).rejects.toThrow(
+        new NotFoundException('Tenant not found!'),
+      );
+    });
+  });
+});
