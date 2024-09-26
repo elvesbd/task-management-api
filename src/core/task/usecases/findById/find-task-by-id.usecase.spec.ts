9163e9c0873deb6adf2dfae88c0c35f151aa4d24
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:03:08 -0300

    test: should throw a NotFoundException if tenant is not found

--- src/core/task/usecases/findById/find-task-by-id.usecase.spec.ts
+++ src/core/task/usecases/findById/find-task-by-id.usecase.spec.ts
@@ -63,5 +63,13 @@ describe('FindTaskByIdUseCase', () => {
       expect(tenantRepository.findById).toHaveBeenCalledTimes(1);
       expect(tenantRepository.findById).toHaveBeenCalledWith(input.tenantId);
     });
+
+    it('should throw a NotFoundException if tenant is not found', async () => {
+      jest.spyOn(tenantRepository, 'findById').mockResolvedValueOnce(null);
+
+      await expect(sut.execute(input)).rejects.toThrow(
+        new NotFoundException(`Tenant not found for ID: ${input.tenantId}`),
+      );
+    });
   });
 });
