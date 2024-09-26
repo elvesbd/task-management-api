9eb8be4de3634024e71cfcb1d155fd3b86eac387
elvesbd <elvesbd@yahoo.com>
2024-09-26 08:35:46 -0300

    test: should call tenantRepository findAll once

---
+++ src/core/tenant/usecases/findAll/find-all-tenant.usecase.spec.ts
@@ -0,0 +1,44 @@
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { FindAllTenantUseCase } from '@core/tenant/usecases';
+import { TenantRepository } from '@core/tenant/ports/repository';
+import { TenantDataBuilder } from '@test/__mocks__/data-builder/tenant';
+
+describe('FindAllTenantUseCase', () => {
+  let sut: FindAllTenantUseCase;
+  let tenantRepository: TenantRepository;
+
+  const tenant = TenantDataBuilder.anTenant().withId().build();
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const TenantRepositoryProvider = {
+      provide: TenantRepository,
+      useValue: {
+        findAll: jest.fn().mockResolvedValue([tenant]),
+      },
+    };
+
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [FindAllTenantUseCase, TenantRepositoryProvider],
+    }).compile();
+
+    sut = app.get<FindAllTenantUseCase>(FindAllTenantUseCase);
+    tenantRepository = app.get<TenantRepository>(TenantRepository);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(tenantRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    it('should call tenantRepository findAll once', async () => {
+      await sut.execute();
+
+      expect(tenantRepository.findAll).toHaveBeenCalledTimes(1);
+      expect(tenantRepository.findAll).toHaveBeenCalledWith();
+    });
+  });
+});
