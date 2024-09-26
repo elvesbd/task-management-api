5eb0860e0e71a74903d9ea023c8be35e92286ddc
elvesbd <elvesbd@yahoo.com>
2024-09-25 21:04:47 -0300

    test: should throw NotFoundException if tenant not found

---
+++ src/core/tenant/usecases/update/update-tenant.usecase.spec.ts
@@ -0,0 +1,53 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { Tenant } from '@core/tenant/model';
+import { UpdateTenantUseCase } from '@core/tenant/usecases';
+import { TenantRepository } from '@core/tenant/ports/repository';
+import { TenantDataBuilder } from '@test/__mocks__/data-builder/tenant';
+
+describe('UpdateTenantUseCase', () => {
+  let sut: UpdateTenantUseCase;
+  let tenantRepository: TenantRepository;
+
+  const input = TenantDataBuilder.anTenant().build();
+  const tenant = Tenant.create(input);
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const TenantRepositoryProvider = {
+      provide: TenantRepository,
+      useValue: {
+        save: jest.fn().mockResolvedValue(0),
+        findByDocument: jest.fn().mockResolvedValue(tenant),
+      },
+    };
+
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [UpdateTenantUseCase, TenantRepositoryProvider],
+    }).compile();
+
+    sut = app.get<UpdateTenantUseCase>(UpdateTenantUseCase);
+    tenantRepository = app.get<TenantRepository>(TenantRepository);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(tenantRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    const tenantId = '019229dc-e8c6-72cc-b599-c938df401967';
+
+    it('should throw NotFoundException if tenant not found', async () => {
+      jest
+        .spyOn(tenantRepository, 'findByDocument')
+        .mockResolvedValueOnce(null);
+
+      await expect(sut.execute({ ...input, id: tenantId })).rejects.toThrow(
+        new NotFoundException('Tenant not found!'),
+      );
+    });
+  });
+});
