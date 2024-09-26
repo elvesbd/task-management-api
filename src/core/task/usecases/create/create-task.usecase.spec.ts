05b0ae145d6d692259118390017b7be727815c16
elvesbd <elvesbd@yahoo.com>
2024-09-26 13:03:25 -0300

    chore: should throw a NotFoundException if tenant is not found

--- src/core/task/usecases/create/create-task.usecase.spec.ts
+++ src/core/task/usecases/create/create-task.usecase.spec.ts
@@ -60,5 +60,13 @@ describe('CreateTaskUseCase', () => {
       expect(tenantRepository.findById).toHaveBeenCalledTimes(1);
       expect(tenantRepository.findById).toHaveBeenCalledWith(input.tenantId);
     });
+
+    it('should throw a NotFoundException if tenant is not found', async () => {
+      jest.spyOn(tenantRepository, 'findById').mockResolvedValueOnce(null);
+
+      await expect(sut.execute(input)).rejects.toThrow(
+        new NotFoundException(`Tenant not found for ID: ${input.tenantId}`),
+      );
+    });
   });
 });
