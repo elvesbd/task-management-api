6d9f818faf0c822048d647b0e82971e9a209e534
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:46:45 -0300

    chore: adding validation for tenant

--- src/core/user/usecases/findAll/find-all-user.usecase.ts
+++ src/core/user/usecases/findAll/find-all-user.usecase.ts
@@ -3,16 +3,24 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { User } from '@core/user/model';
 import { UseCase } from '@core/shared/contracts/usecases';
 import { UserRepository } from '@core/user/ports/repository';
+import { TenantRepository } from '@core/tenant/ports/repository';
 
 @Injectable()
 export class FindAllUserUseCase implements UseCase<null, User[]> {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly tenantRepository: TenantRepository,
+  ) {}
 
   async execute(tenantId: string): Promise<User[]> {
-    const users = await this.userRepository.findAll(tenantId);
+    const tenant = await this.tenantRepository.findById(tenantId);
+    if (!tenant)
+      throw new NotFoundException(`Tenant not found for ID: ${tenantId}`);
+
+    const users = await this.userRepository.findAll(tenant.id);
 
     if (!users || users.length === 0) {
-      throw new NotFoundException(`No users found for tenant ID: ${tenantId}`);
+      throw new NotFoundException(`No users found for tenant ID: ${tenant.id}`);
     }
 
     return users;
