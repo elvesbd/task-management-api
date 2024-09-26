287f088ec4e3e0c6c88bc19a159ab99bc2dd32d2
elvesbd <elvesbd@yahoo.com>
2024-09-26 20:07:27 -0300

    test: should throw NotFoundException if user does not exist

---
+++ src/core/user/usecases/update-password/update-user-password.usecase.spec.ts
@@ -0,0 +1,73 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
+
+import { User } from '@core/user/model';
+import { UserRepository } from '@core/user/ports/repository';
+import { UpdateUserPasswordUseCase } from '@core/user/usecases';
+import { UserDataBuilder } from '@test/__mocks__/data-builder/user';
+import { PasswordEncryption } from '@core/authentication/ports/encryption';
+
+describe('UpdateUserPasswordUseCase', () => {
+  let sut: UpdateUserPasswordUseCase;
+  let userRepository: UserRepository;
+  let passwordEncryption: PasswordEncryption;
+
+  const input = {
+    email: 'user@mail.com',
+    currentPassword: 'securePassword',
+    newPassword: 'newPassword123',
+  };
+
+  const userMock = UserDataBuilder.aUser()
+    .withPassword('hashedOldPassword')
+    .build();
+  const user = User.create(userMock);
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const UserRepositoryProvider = {
+      provide: UserRepository,
+      useValue: {
+        save: jest.fn(),
+        findByEmail: jest.fn().mockResolvedValue(user),
+      },
+    };
+
+    const PasswordEncryptionProvider = {
+      provide: PasswordEncryption,
+      useValue: {
+        compare: jest.fn().mockResolvedValue(true),
+        hash: jest.fn().mockResolvedValue('hashedNewPassword'),
+      },
+    };
+
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        UpdateUserPasswordUseCase,
+        UserRepositoryProvider,
+        PasswordEncryptionProvider,
+      ],
+    }).compile();
+
+    sut = app.get<UpdateUserPasswordUseCase>(UpdateUserPasswordUseCase);
+    userRepository = app.get<UserRepository>(UserRepository);
+    passwordEncryption = app.get<PasswordEncryption>(PasswordEncryption);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(userRepository).toBeDefined();
+    expect(passwordEncryption).toBeDefined();
+  });
+
+  describe('execute', () => {
+    it('should throw NotFoundException if user does not exist', async () => {
+      jest.spyOn(userRepository, 'findByEmail').mockResolvedValueOnce(null);
+
+      await expect(sut.execute(input)).rejects.toThrow(
+        new NotFoundException('User not found!'),
+      );
+    });
+  });
+});
