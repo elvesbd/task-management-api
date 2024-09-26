7d641ad7888dbd6dfa8d9a6b1bcf8014e1da4672
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:46:54 -0300

    test: adding tests that ensure the correct tenant

--- src/core/user/usecases/findAll/find-all-user.usecase.spec.ts
+++ src/core/user/usecases/findAll/find-all-user.usecase.spec.ts
@@ -3,52 +3,83 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { FindAllUserUseCase } from '@core/user/usecases';
 import { UserRepository } from '@core/user/ports/repository';
+import { TenantRepository } from '@core/tenant/ports/repository';
 import { UserDataBuilder } from '@test/__mocks__/data-builder/user';
 
 describe('FindAllUserUseCase', () => {
   let sut: FindAllUserUseCase;
   let userRepository: UserRepository;
+  let tenantRepository: TenantRepository;
 
   const user = UserDataBuilder.aUser().withId().build();
+  const tenant = UserDataBuilder.aUser().withId().build();
 
   beforeEach(async () => {
     jest.clearAllMocks();
 
-    const TenantRepositoryProvider = {
+    const UserRepositoryProvider = {
       provide: UserRepository,
       useValue: {
         findAll: jest.fn().mockResolvedValue([user]),
       },
     };
 
+    const TenantRepositoryProvider = {
+      provide: TenantRepository,
+      useValue: {
+        findById: jest.fn().mockResolvedValue(tenant),
+      },
+    };
+
     const app: TestingModule = await Test.createTestingModule({
-      providers: [FindAllUserUseCase, TenantRepositoryProvider],
+      providers: [
+        FindAllUserUseCase,
+        UserRepositoryProvider,
+        TenantRepositoryProvider,
+      ],
     }).compile();
 
     sut = app.get<FindAllUserUseCase>(FindAllUserUseCase);
     userRepository = app.get<UserRepository>(UserRepository);
+    tenantRepository = app.get<TenantRepository>(TenantRepository);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
     expect(userRepository).toBeDefined();
+    expect(tenantRepository).toBeDefined();
   });
 
   describe('execute', () => {
     const tenantId = '019229dc-e8c6-72cc-b599-c938df401967';
 
+    it('should call tenantRepository findById once', async () => {
+      await sut.execute(tenantId);
+
+      expect(tenantRepository.findById).toHaveBeenCalledTimes(1);
+      expect(tenantRepository.findById).toHaveBeenCalledWith(tenantId);
+    });
+
+    it('should throw a NotFoundException if tenant is not found', async () => {
+      jest.spyOn(tenantRepository, 'findById').mockResolvedValueOnce(null);
+
+      await expect(sut.execute(tenantId)).rejects.toThrow(
+        new NotFoundException(`Tenant not found for ID: ${tenantId}`),
+      );
+    });
+
     it('should call userRepository findAll once', async () => {
       await sut.execute(tenantId);
 
       expect(userRepository.findAll).toHaveBeenCalledTimes(1);
-      expect(userRepository.findAll).toHaveBeenCalledWith(tenantId);
+      expect(userRepository.findAll).toHaveBeenCalledWith(tenant.id);
     });
 
     it('should throw a NotFoundException if returns empty array', async () => {
       jest.spyOn(userRepository, 'findAll').mockResolvedValueOnce([]);
 
       await expect(sut.execute(tenantId)).rejects.toThrow(
-        new NotFoundException(`No users found for tenant ID: ${tenantId}`),
+        new NotFoundException(`No users found for tenant ID: ${tenant.id}`),
       );
     });
 
@@ -56,7 +87,7 @@ describe('FindAllUserUseCase', () => {
       jest.spyOn(userRepository, 'findAll').mockResolvedValueOnce(undefined);
 
       await expect(sut.execute(tenantId)).rejects.toThrow(
-        new NotFoundException(`No users found for tenant ID: ${tenantId}`),
+        new NotFoundException(`No users found for tenant ID: ${tenant.id}`),
       );
     });
 
