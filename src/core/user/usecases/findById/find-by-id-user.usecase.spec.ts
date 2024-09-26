d1bceec3cab09fd606ded4e3e7b6573309b87e8f
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:50:37 -0300

    test: adding tests that ensure the correct tenant

--- src/core/user/usecases/findById/find-by-id-user.usecase.spec.ts
+++ src/core/user/usecases/findById/find-by-id-user.usecase.spec.ts
@@ -3,13 +3,17 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import { FindByIdUserUseCase } from '@core/user/usecases';
 import { UserRepository } from '@core/user/ports/repository';
+import { TenantRepository } from '@core/tenant/ports/repository';
 import { UserDataBuilder } from '@test/__mocks__/data-builder/user';
+import { TenantDataBuilder } from '@test/__mocks__/data-builder/tenant';
 
 describe('FindByIdUserUseCase', () => {
   let sut: FindByIdUserUseCase;
   let userRepository: UserRepository;
+  let tenantRepository: TenantRepository;
 
   const user = UserDataBuilder.aUser().withId().build();
+  const tenant = TenantDataBuilder.anTenant().withId().build();
 
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -21,17 +25,30 @@ describe('FindByIdUserUseCase', () => {
       },
     };
 
+    const TenantRepositoryProvider = {
+      provide: TenantRepository,
+      useValue: {
+        findById: jest.fn().mockResolvedValue(tenant),
+      },
+    };
+
     const app: TestingModule = await Test.createTestingModule({
-      providers: [FindByIdUserUseCase, UserRepositoryProvider],
+      providers: [
+        FindByIdUserUseCase,
+        UserRepositoryProvider,
+        TenantRepositoryProvider,
+      ],
     }).compile();
 
     sut = app.get<FindByIdUserUseCase>(FindByIdUserUseCase);
     userRepository = app.get<UserRepository>(UserRepository);
+    tenantRepository = app.get<TenantRepository>(TenantRepository);
   });
 
   it('should be defined', () => {
     expect(sut).toBeDefined();
     expect(userRepository).toBeDefined();
+    expect(tenantRepository).toBeDefined();
   });
 
   describe('execute', () => {
@@ -40,13 +57,28 @@ describe('FindByIdUserUseCase', () => {
       tenantId: '019229dc-e8c6-72cc-b599-c938df401967',
     };
 
+    it('should call tenantRepository findById once', async () => {
+      await sut.execute(input);
+
+      expect(tenantRepository.findById).toHaveBeenCalledTimes(1);
+      expect(tenantRepository.findById).toHaveBeenCalledWith(input.tenantId);
+    });
+
+    it('should throw a NotFoundException if tenant is not found', async () => {
+      jest.spyOn(tenantRepository, 'findById').mockResolvedValueOnce(null);
+
+      await expect(sut.execute(input)).rejects.toThrow(
+        new NotFoundException(`Tenant not found for ID: ${input.tenantId}`),
+      );
+    });
+
     it('should call userRepository findById once', async () => {
       await sut.execute(input);
 
       expect(userRepository.findByIdAndTenantId).toHaveBeenCalledTimes(1);
       expect(userRepository.findByIdAndTenantId).toHaveBeenCalledWith(
         input.id,
-        input.tenantId,
+        tenant.id,
       );
     });
 
@@ -56,7 +88,7 @@ describe('FindByIdUserUseCase', () => {
         .mockResolvedValueOnce(null);
 
       await expect(sut.execute(input)).rejects.toThrow(
-        new NotFoundException(`User not found for ID: ${input.tenantId}`),
+        new NotFoundException(`User not found for ID: ${tenant.id}`),
       );
     });
 
