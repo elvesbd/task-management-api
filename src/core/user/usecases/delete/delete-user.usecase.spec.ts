55d706de630a050b3e505bb59a4cfd4b76835b21
elvesbd <elvesbd@yahoo.com>
2024-09-26 12:03:27 -0300

    test: should call userRepository findByIdAndTenantId once

---
+++ src/core/user/usecases/delete/delete-user.usecase.spec.ts
@@ -0,0 +1,54 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { UserRepository } from '@core/user/ports/repository';
+import { UserDataBuilder } from '@test/__mocks__/data-builder/user';
+import { DeleteUserUseCase } from './delete-user.usecase';
+
+describe('DeleteUserUseCase', () => {
+  let sut: DeleteUserUseCase;
+  let userRepository: UserRepository;
+
+  const user = UserDataBuilder.aUser().withId().build();
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const UserRepositoryProvider = {
+      provide: UserRepository,
+      useValue: {
+        findByIdAndTenantId: jest.fn().mockResolvedValue(user),
+        delete: jest.fn(),
+      },
+    };
+
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [DeleteUserUseCase, UserRepositoryProvider],
+    }).compile();
+
+    sut = app.get<DeleteUserUseCase>(DeleteUserUseCase);
+    userRepository = app.get<UserRepository>(UserRepository);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(userRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    const input = {
+      id: '086229dc-e8c6-72cc-b599-c938df401741',
+      tenantId: '019229dc-e8c6-72cc-b599-c938df401967',
+    };
+
+    it('should call userRepository findByIdAndTenantId once', async () => {
+      await sut.execute(input);
+
+      expect(userRepository.findByIdAndTenantId).toHaveBeenCalledTimes(1);
+      expect(userRepository.findByIdAndTenantId).toHaveBeenCalledWith(
+        input.id,
+        input.tenantId,
+      );
+    });
+  });
+});
