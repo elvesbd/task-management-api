e5c99aa93c2d3df3b0bacf07344e128420518890
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:39:43 -0300

    chore: adding validation for tenant

--- src/core/user/usecases/delete/delete-user.usecase.ts
+++ src/core/user/usecases/delete/delete-user.usecase.ts
@@ -2,6 +2,7 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 
 import { UseCase } from '@core/shared/contracts/usecases';
 import { UserRepository } from '@core/user/ports/repository';
+import { TenantRepository } from '@core/tenant/ports/repository';
 
 type Input = {
   id: string;
@@ -10,20 +11,25 @@ type Input = {
 
 @Injectable()
 export class DeleteUserUseCase implements UseCase<Input, void> {
-  constructor(private readonly userRepository: UserRepository) {}
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly tenantRepository: TenantRepository,
+  ) {}
 
   async execute(input: Input): Promise<void> {
+    const tenant = await this.tenantRepository.findById(input.tenantId);
+    if (!tenant)
+      throw new NotFoundException(`Tenant not found for ID: ${input.tenantId}`);
+
     const user = await this.userRepository.findByIdAndTenantId(
       input.id,
-      input.tenantId,
+      tenant.id,
     );
 
     if (!user) {
-      throw new NotFoundException(
-        `User not found for tenant ID: ${input.tenantId}`,
-      );
+      throw new NotFoundException(`User not found for tenant ID: ${tenant.id}`);
     }
 
-    await this.userRepository.delete(input.id);
+    await this.userRepository.delete(tenant.id);
   }
 }
