60a37cab381be8c558b34ae8bce800e9b7a4d6d2
elvesbd <elvesbd@yahoo.com>
2024-09-26 16:23:24 -0300

    test: should call tenantRepository findById once

---
+++ src/core/task/usecases/delete/delete-task.usecase.spec.ts
@@ -0,0 +1,68 @@
+import { NotFoundException } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
+
+import { DeleteTaskUseCase } from '@core/task/usecases';
+import { TaskRepository } from '@core/task/ports/repository';
+import { TenantRepository } from '@core/tenant/ports/repository';
+import { TaskDataBuilder } from '@test/__mocks__/data-builder/task';
+import { TenantDataBuilder } from '@test/__mocks__/data-builder/tenant';
+
+describe('DeleteTaskUseCase', () => {
+  let sut: DeleteTaskUseCase;
+  let taskRepository: TaskRepository;
+  let tenantRepository: TenantRepository;
+
+  const task = TaskDataBuilder.aTask().withId().build();
+  const tenant = TenantDataBuilder.anTenant().withId().build();
+
+  beforeEach(async () => {
+    jest.clearAllMocks();
+
+    const TaskRepositoryProvider = {
+      provide: TaskRepository,
+      useValue: {
+        delete: jest.fn(),
+        findByIdAndTenantId: jest.fn().mockResolvedValue(task),
+      },
+    };
+
+    const TenantRepositoryProvider = {
+      provide: TenantRepository,
+      useValue: {
+        findById: jest.fn().mockResolvedValue(tenant),
+      },
+    };
+
+    const app: TestingModule = await Test.createTestingModule({
+      providers: [
+        DeleteTaskUseCase,
+        TaskRepositoryProvider,
+        TenantRepositoryProvider,
+      ],
+    }).compile();
+
+    sut = app.get<DeleteTaskUseCase>(DeleteTaskUseCase);
+    taskRepository = app.get<TaskRepository>(TaskRepository);
+    tenantRepository = app.get<TenantRepository>(TenantRepository);
+  });
+
+  it('should be defined', () => {
+    expect(sut).toBeDefined();
+    expect(taskRepository).toBeDefined();
+    expect(tenantRepository).toBeDefined();
+  });
+
+  describe('execute', () => {
+    const input = {
+      id: '086229dc-e8c6-72cc-b599-c938df401741',
+      tenantId: '019229dc-e8c6-72cc-b599-c938df401967',
+    };
+
+    it('should call tenantRepository findById once', async () => {
+      await sut.execute(input);
+
+      expect(tenantRepository.findById).toHaveBeenCalledTimes(1);
+      expect(tenantRepository.findById).toHaveBeenCalledWith(input.tenantId);
+    });
+  });
+});
