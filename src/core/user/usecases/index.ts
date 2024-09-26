ca9f8c2737bf635c828bf732cdb06205c710c0c2
elvesbd <elvesbd@yahoo.com>
2024-09-26 20:06:56 -0300

    chore: implementing update password use case

--- src/core/user/usecases/index.ts
+++ src/core/user/usecases/index.ts
@@ -3,3 +3,4 @@ export * from './create/create-user.usecase';
 export * from './update/update-user.usecase';
 export * from './findAll/find-all-user.usecase';
 export * from './findById/find-by-id-user.usecase';
+export * from './update-password/update-user-password.usecase';
