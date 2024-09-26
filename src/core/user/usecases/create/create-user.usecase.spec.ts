6a3e75dce07a0800771e410dbc347e1cf6c74663
elvesbd <elvesbd@yahoo.com>
2024-09-26 19:23:06 -0300

    test: should hash the password before saving the user

--- src/core/user/usecases/create/create-user.usecase.spec.ts
+++ src/core/user/usecases/create/create-user.usecase.spec.ts
@@ -5,13 +5,16 @@ import { User } from '@core/user/model';
 import { CreateUserUseCase } from '@core/user/usecases';
 import { UserRepository } from '@core/user/ports/repository';
 import { UserDataBuilder } from '@test/__mocks__/data-builder/user';
+import { PasswordEncryption } from '@core/authentication/ports/encryption';
 
 describe('CreateUserUseCase', () => {
   let sut: CreateUserUseCase;
   let userRepository: UserRepository;
+  let passwordEncryption: PasswordEncryption;
 
   const input = UserDataBuilder.aUser().build();
   const user = User.create(input);
+  const hashedPassword = 'hashedPassword';
 
   beforeEach(async () => {
     jest.clearAllMocks();
@@ -24,17 +27,30 @@ describe('CreateUserUseCase', () => {
       },
     };
 
+    const PasswordEncryptionProvider = {
+      provide: PasswordEncryption,
+      useValue: {
+        hash: jest.fn().mockResolvedValue(hashedPassword),
+      },
+    };
+
     const app: TestingModule = await Test.createTestingModule({
-      providers: [CreateUserUseCase, UserRepositoryProvider],
+      providers: [
+        CreateUserUseCase,
+        UserRepositoryProvider,
+        PasswordEncryptionProvider,
+      ],
     }).compile();
 
     sut = app.get<CreateUserUseCase>(CreateUserUseCase);
     userRepository = app.get<UserRepository>(UserRepository);
+    passwordEncryption = app.get<PasswordEncryption>(PasswordEncryption);
   });
 
   it('should defined', () => {
     expect(sut).toBeDefined();
     expect(userRepository).toBeDefined();
+    expect(passwordEncryption).toBeDefined();
   });
 
   describe('execute', () => {
@@ -44,6 +60,13 @@ describe('CreateUserUseCase', () => {
       await expect(sut.execute(input)).rejects.toThrow(
         new BadRequestException('User already exists with this email!'),
       );
+    });
+
+    it('should hash the password before saving the user', async () => {
+      await sut.execute(input);
+
+      expect(passwordEncryption.hash).toHaveBeenCalledTimes(1);
+      expect(passwordEncryption.hash).toHaveBeenCalledWith(input.password);
     });
 
     it('should call tenantRepository save on success', async () => {
@@ -56,10 +79,11 @@ describe('CreateUserUseCase', () => {
     it('should create a new user when no existing user is found', async () => {
       const output = await sut.execute(input);
 
-      expect(output.user.id).toBeDefined();
-      expect(output.user.role).toBe(input.role);
-      expect(output.user.email).toBe(input.email);
-      expect(output.user.tenantId).toBe(input.tenantId);
+      expect(output.id).toBeDefined();
+      expect(output.role).toBe(input.role);
+      expect(output.email).toBe(input.email);
+      expect(output.tenantId).toBe(input.tenantId);
+      expect(output.password).toBe(hashedPassword);
     });
   });
 });
