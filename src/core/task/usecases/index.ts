d5bdb15e9b5ba96ad960705dfe1d38a7246b561a
elvesbd <elvesbd@yahoo.com>
2024-09-27 14:54:55 -0300

    chore: implementing controller update tenant

--- src/core/task/usecases/index.ts
+++ src/core/task/usecases/index.ts
@@ -3,3 +3,4 @@ export * from './create/create-task.usecase';
 export * from './update/update-task.usecase';
 export * from './findAll/find-all-task.usecase';
 export * from './findById/find-task-by-id.usecase';
+export * from './update-task-status/update-task-status.usecase';
---
+++ src/infra/http/presenters/controllers/tenant/update/update-tenant.controller.ts
@@ -0,0 +1,49 @@
+import { Controller, Patch, Body, Param } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBody,
+  ApiParam,
+  ApiNotFoundResponse,
+  ApiOkResponse,
+} from '@nestjs/swagger';
+
+import { ApiPath, ApiTag } from '../constants';
+import { UpdateTenantUseCase } from '@core/tenant/usecases';
+import { UpdateTenantDto } from '@infra/http/presenters/controllers/tenant/dtos';
+
+@ApiTags(ApiTag)
+@Controller(ApiPath)
+export class UpdateTenantController {
+  constructor(private readonly updateTenantUseCase: UpdateTenantUseCase) {}
+
+  @ApiOperation({
+    description: 'Update an existing tenant.',
+  })
+  @ApiParam({
+    name: 'id',
+    required: true,
+    description: 'ID of the tenant to be updated.',
+    type: String,
+  })
+  @ApiBody({
+    type: UpdateTenantDto,
+    description: 'Data to update the tenant.',
+  })
+  @ApiOkResponse({
+    description: 'Tenant updated successfully.',
+  })
+  @ApiNotFoundResponse({
+    description: 'Tenant not found.',
+  })
+  @Patch('tenants/:id')
+  public async updateTenant(
+    @Param('id') id: string,
+    @Body() dto: UpdateTenantDto,
+  ): Promise<void> {
+    await this.updateTenantUseCase.execute({
+      id,
+      ...dto,
+    });
+  }
+}
