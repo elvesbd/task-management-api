dce70705f2fc51d42711783c8cd52dfc9ac7761f
elvesbd <elvesbd@yahoo.com>
2024-09-27 14:44:12 -0300

    chore: implementing controller create tenant

---
+++ src/infra/http/presenters/controllers/tenant/create/create-tenant.controller.ts
@@ -0,0 +1,45 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import {
+  ApiTags,
+  ApiOperation,
+  ApiBadRequestResponse,
+  ApiCreatedResponse,
+  ApiBody,
+} from '@nestjs/swagger';
+
+import { ApiPath, ApiTag } from '../constants';
+import { CreateTenantUseCase } from '@core/tenant/usecases';
+import {
+  TenantViewModel,
+  TenantVMResponse,
+} from '@infra/http/presenters/view-models/tenant';
+import { CreateTenantDto } from '@infra/http/presenters/controllers/tenant/dtos';
+
+@ApiTags(ApiTag)
+@Controller(ApiPath)
+export class CreateTenantController {
+  constructor(private readonly createTenantUseCase: CreateTenantUseCase) {}
+
+  @ApiOperation({
+    description: 'Create a new tenant.',
+  })
+  @ApiBody({
+    type: CreateTenantDto,
+    description: 'Data to create a new tenant.',
+  })
+  @ApiCreatedResponse({
+    description: 'Tenant created successfully.',
+    type: TenantViewModel,
+  })
+  @ApiBadRequestResponse({
+    description: 'Bad Request. Tenant already exists with this document.',
+  })
+  @Post('/tenants')
+  public async createTenant(
+    @Body() dto: CreateTenantDto,
+  ): Promise<TenantVMResponse> {
+    const tenant = await this.createTenantUseCase.execute(dto);
+
+    return TenantViewModel.toHTTP(tenant);
+  }
+}
