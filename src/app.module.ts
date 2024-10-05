36d1234722d577b60cf9d13c90c2a593c5f71c79
elvesbd <elvesbd@yahoo.com>
2024-10-05 17:01:03 -0300

    chore: applying rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
-import { DatabaseModule } from '@infra/database';
 import { HttpModule } from '@infra/http';
 import { AuthModule } from '@infra/auth';
 import { APP_GUARD } from '@nestjs/core';
 import { AtGuard } from '@infra/auth/guards';
+import { DatabaseModule } from '@infra/database';
 import { RolesGuard } from '@infra/auth/guards/roles.guard';
 
 @Module({
@@ -13,6 +14,12 @@ import { RolesGuard } from '@infra/auth/guards/roles.guard';
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60, // Tempo de vida (Time to live) em segundos
+        limit: 10, // Número máximo de requisições por período
+      },
+    ]),
     DatabaseModule,
     HttpModule,
     AuthModule,
